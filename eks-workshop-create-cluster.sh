8f514c22e715c9caa136956898f15e126957ed93
FrankieCheng <cheng.onion@gmail.com>
2022-07-21 18:49:34 +0800

    Update eks-workshop-create-cluster.sh

--- eks-workshop-create-cluster.sh
+++ eks-workshop-create-cluster.sh
@@ -51,6 +51,8 @@ export MASTER_ARN=$(aws kms describe-key --key-id alias/eksworkshop --query KeyM
 
 echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
 
+source  ~/.bash_profile
+
 #install eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 

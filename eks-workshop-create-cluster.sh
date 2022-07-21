86b526a42b472b457bee03a2459a70f117815762
FrankieCheng <cheng.onion@gmail.com>
2022-07-21 18:35:08 +0800

    Update eks-workshop-create-cluster.sh

--- eks-workshop-create-cluster.sh
+++ eks-workshop-create-cluster.sh
@@ -5,8 +5,8 @@ aws ec2 associate-iam-instance-profile --instance-id $instance_id --iam-instance
 
 #update cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-#unzip awscliv2.zip
-#sudo ./aws/install
+unzip awscliv2.zip
+sudo ./aws/install
 
 #install jq.....
 sudo yum -y install jq gettext bash-completion moreutils

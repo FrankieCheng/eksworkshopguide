1449b19d2fe4aeb060c6c6b74d3d3b742811624b
FrankieCheng <cheng.onion@gmail.com>
2022-07-22 10:38:54 +0800

    Update eks-workshop-create-cluster.sh

--- eks-workshop-create-cluster.sh
+++ eks-workshop-create-cluster.sh
@@ -73,3 +73,11 @@ sed 's/$AWS_REGION/'"${AWS_REGION}"'/g;s/$AZ0/'"${AZS[0]}"'/g;s/$AZ1/'"${AZS[1]}
 
 # create cluster.
 eksctl create cluster -f eksworkshopguide/yamls/ekscluster.yaml
+
+kubectl get nodes # if we see our 3 nodes, we know we have authenticated correctly
+
+aws eks update-kubeconfig --name eksworkshop-eksctl --region ${AWS_REGION}
+
+STACK_NAME=$(eksctl get nodegroup --cluster eksworkshop-eksctl -o json | jq -r '.[].StackName')
+ROLE_NAME=$(aws cloudformation describe-stack-resources --stack-name $STACK_NAME | jq -r '.StackResources[] | select(.ResourceType=="AWS::IAM::Role") | .PhysicalResourceId')
+echo "export ROLE_NAME=${ROLE_NAME}" | tee -a ~/.bash_profile

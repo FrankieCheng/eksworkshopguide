899a4b0661882b29430396fd1f86514510ecac25
FrankieCheng <cheng.onion@gmail.com>
2022-07-22 12:53:47 +0800

    Update eks-workshop-create-cluster.sh

--- eks-workshop-create-cluster.sh
+++ eks-workshop-create-cluster.sh
@@ -80,3 +80,18 @@ aws eks update-kubeconfig --name eksworkshop-eksctl --region ${AWS_REGION}
 STACK_NAME=$(eksctl get nodegroup --cluster eksworkshop-eksctl -o json | jq -r '.[].StackName')
 ROLE_NAME=$(aws cloudformation describe-stack-resources --stack-name $STACK_NAME | jq -r '.StackResources[] | select(.ResourceType=="AWS::IAM::Role") | .PhysicalResourceId')
 echo "export ROLE_NAME=${ROLE_NAME}" | tee -a ~/.bash_profile
+
+#EKS Console credentials to your new cluster
+c9builder=$(aws cloud9 describe-environment-memberships --environment-id=$C9_PID | jq -r '.memberships[].userArn')
+if echo ${c9builder} | grep -q user; then
+	rolearn=${c9builder}
+        echo Role ARN: ${rolearn}
+elif echo ${c9builder} | grep -q assumed-role; then
+        assumedrolename=$(echo ${c9builder} | awk -F/ '{print $(NF-1)}')
+        rolearn=$(aws iam get-role --role-name ${assumedrolename} --query Role.Arn --output text) 
+        echo Role ARN: ${rolearn}
+fi
+
+eksctl create iamidentitymapping --cluster eksworkshop-eksctl --arn ${rolearn} --group system:masters --username admin
+
+kubectl describe configmap -n kube-system aws-auth

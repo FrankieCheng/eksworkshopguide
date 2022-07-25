8578f6089142c4995637af47c92f8aedbc011392
FrankieCheng <cheng.onion@gmail.com>
2022-07-25 14:06:13 +0800

    add test cluster

--- eks-workshop-bottlerocket.sh
+++ eks-workshop-bottlerocket.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #bottlerocket
+#create keypair
+aws ec2 create-key-pair \
+    --key-name eksbottlerocketkeypair \
+    --key-type rsa \
+    --key-format pem \
+    --query "KeyMaterial" \
+    --output text > bottlerocket-key-pair.pem
 
 #replace the parameter for nodegroup template.
 sed 's/$AWS_REGION/'"${AWS_REGION}"'/g' eksworkshopguide/yamls/ekscluster-nodegroup-bottlerocket-template.yaml > eksworkshopguide/yamls/ekscluster-nodegroup-bottlerocket.yaml
